94246e43601817b205cecf265c193fb2544ffb68
David LaPalomento <dlapalomento@gmail.com>
2015-10-25 13:38:47 -0400

    Split pane diff view Simple unified diffs can get out of sync and make it very difficult to compare equivalent sections of mp4 output. Pull in jsdifflib to provide a split panel diff that may be easier to debug with.

--- debug/js/main.js
+++ debug/js/main.js
@@ -1,1 +1,23 @@
+(function(muxjs) {
+  'use strict';
 
+  var diffParsed = function(expected, actual) {
+    var expectedLines = muxjs.tools.textifyMp4(expected, null, ' ').split('\n');
+    var actualLines = muxjs.tools.textifyMp4(actual, null, ' ').split('\n');
+    var matcher = new difflib.SequenceMatcher(expectedLines, actualLines);
+
+    return diffview.buildView({
+      baseTextLines: expectedLines,
+      newTextLines: actualLines,
+      opcodes: matcher.get_opcodes(),
+      baseTextName: "Expected MP4",
+      newTextName: "Actual MP4",
+      contextSize: 10,
+      viewType: 0
+    });
+  };
+
+  muxjs.debug = {
+    diffParsed: diffParsed
+  };
+})(window.muxjs);
