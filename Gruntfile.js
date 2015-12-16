2a840c623f1987c5818913e924a04e7c3140caac
brandonocasey <brandonocasey@gmail.com>
2015-12-16 10:37:30 -0500

    rebasing old node module conversion with new mux.js changes

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    browserify: {
+      options: {
+        browserifyOptions: {
+          debug: true,
+          standalone: 'muxjs'
+        },
+        plugin: [
+          ['browserify-derequire']
+        ],
+        transform: [
+          require('babelify').configure({
+            sourceMapRelative: './',
+            loose: ['all']
+          })
+        ]
+      },
+      build: {
+        files: {
+          'dist/mux.js': ['lib/mux.js']
+        }
+      },
+      watch: {
+        options: {
+          watch: true,
+          keepAlive: true
+        },
+        files: {
+          'dist/mux.js': ['lib/mux.js']
+        }
+      }
+    },
+    jshint: {
+      options: {
+        browserify: true,
+        node: true,
+        browser: true
+      },
+      src: './lib'
+    }
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('build', ['jshint', 'browserify:build']);
+  grunt.registerTask('watch', ['browserify:watch']);
+};
