e39f1f92c7043c1d4d594c02ff6f1cf82c8f7ed0
brandonocasey <brandonocasey@gmail.com>
2015-12-17 11:16:32 -0500

    now tests firefox and chrome

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -66,7 +66,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: process.env.TRAVIS ? ['Firefox'] : ['Chrome'],
+    browsers: process.env.TRAVIS ? ['Firefox'] : ['Chrome', 'Firefox'],
 
 
     // Continuous Integration mode
