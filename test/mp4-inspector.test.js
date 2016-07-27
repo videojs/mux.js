5d4aeda20969b2c1e41b886a2ada0e1fff8cbddd
David LaPalomento <dlapalomento@gmail.com>
2016-07-27 16:25:28 -0400

    MP4 Probe (#103)
    * Add support for v2 tfdt
    Correctly parse tfdts with 64-bit fields. We can't avoid the possibility of overflow but values that aren't too big for js numbers should work.
    
    * Utility for probing start times from MP4s
    Add functions to extract track timescales and base media decode times from fragmented MP4s. This is intended to be used to correctly set the timestampOffset when playing back MP4s that have a non-zero base media decode time to start with. Re-arrange some test utility functions to avoid duplicating sample test data.

--- test/mp4-inspector.test.js
+++ test/mp4-inspector.test.js
@@ -21,53 +21,11 @@
 */
 var
   mp4 = require('../lib/mp4'),
+  mp4Helpers = require('./utils/mp4-helpers'),
   QUnit = require('qunit'),
-  typeBytes = function(type) {
-    return [
-      type.charCodeAt(0),
-      type.charCodeAt(1),
-      type.charCodeAt(2),
-      type.charCodeAt(3)
-    ];
-  },
-  box = function(type) {
-    var
-      array = Array.prototype.slice.call(arguments, 1),
-      result = [],
-      size,
-      i;
-
-    // "unwrap" any arrays that were passed as arguments
-    // e.g. box('etc', 1, [2, 3], 4) -> box('etc', 1, 2, 3, 4)
-    for (i = 0; i < array.length; i++) {
-      if (array[i] instanceof Array) {
-        array.splice.apply(array, [i, 1].concat(array[i]));
-      }
-    }
-
-    size = 8 + array.length;
-
-    result[0] = (size & 0xFF000000) >> 24;
-    result[1] = (size & 0x00FF0000) >> 16;
-    result[2] = (size & 0x0000FF00) >> 8;
-    result[3] = size & 0xFF;
-    result = result.concat(typeBytes(type));
-    result = result.concat(array);
-    return result;
-  },
-  unityMatrix = [
-    0, 0, 0x10, 0,
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-
-    0, 0, 0, 0,
-    0, 0, 0x10, 0,
-    0, 0, 0, 0,
-
-    0, 0, 0, 0,
-    0, 0, 0, 0,
-    0x40, 0, 0, 0
-  ],
+  typeBytes = mp4Helpers.typeBytes,
+  box = mp4Helpers.box,
+  unityMatrix = mp4Helpers.unityMatrix,
 
   mvhd0 = box('mvhd',
              0x00, // version 0
@@ -251,107 +209,7 @@ QUnit.test('can parse a version 0 mdhd', function() {
 });
 
 QUnit.test('can parse a moov', function() {
-  var data =
-    box('moov',
-        box('mvhd',
-            0x01, // version 1
-            0x00, 0x00, 0x00, // flags
-            0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x01, // creation_time
-            0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x02, // modification_time
-            0x00, 0x00, 0x00, 0x3c, // timescale
-            0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x02, 0x58, // 600 = 0x258 duration
-            0x00, 0x01, 0x00, 0x00, // 1.0 rate
-            0x01, 0x00, // 1.0 volume
-            0x00, 0x00, // reserved
-            0x00, 0x00, 0x00, 0x00, // reserved
-            0x00, 0x00, 0x00, 0x00, // reserved
-            unityMatrix,
-            0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, // pre_defined
-            0x00, 0x00, 0x00, 0x02), // next_track_ID
-        box('trak',
-            box('tkhd',
-                0x01, // version 1
-                0x00, 0x00, 0x00, // flags
-                0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x02, // creation_time
-                0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x03, // modification_time
-                0x00, 0x00, 0x00, 0x01, // track_ID
-                0x00, 0x00, 0x00, 0x00, // reserved
-                0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x02, 0x58, // 600 = 0x258 duration
-                0x00, 0x00, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, // reserved
-                0x00, 0x00, // layer
-                0x00, 0x00, // alternate_group
-                0x00, 0x00, // non-audio track volume
-                0x00, 0x00, // reserved
-                unityMatrix,
-                0x01, 0x2c, 0x00, 0x00, // 300 in 16.16 fixed-point
-                0x00, 0x96, 0x00, 0x00), // 150 in 16.16 fixed-point
-            box('mdia',
-                box('mdhd',
-                    0x01, // version 1
-                    0x00, 0x00, 0x00, // flags
-                    0x00, 0x00, 0x00, 0x00,
-                    0x00, 0x00, 0x00, 0x02, // creation_time
-                    0x00, 0x00, 0x00, 0x00,
-                    0x00, 0x00, 0x00, 0x03, // modification_time
-                    0x00, 0x00, 0x00, 0x3c, // timescale
-                    0x00, 0x00, 0x00, 0x00,
-                    0x00, 0x00, 0x02, 0x58, // 600 = 0x258 duration
-                    0x15, 0xc7, // 'eng' language
-                    0x00, 0x00),
-                box('hdlr',
-                    0x01, // version 1
-                    0x00, 0x00, 0x00, // flags
-                    0x00, 0x00, 0x00, 0x00, // pre_defined
-                    typeBytes('vide'), // handler_type
-                    0x00, 0x00, 0x00, 0x00, // reserved
-                    0x00, 0x00, 0x00, 0x00, // reserved
-                    0x00, 0x00, 0x00, 0x00, // reserved
-                    typeBytes('one'), 0x00), // name
-                box('minf',
-                    box('dinf',
-                        box('dref',
-                            0x01, // version 1
-                            0x00, 0x00, 0x00, // flags
-                            0x00, 0x00, 0x00, 0x01, // entry_count
-                            box('url ',
-                            0x00, // version
-                            0x00, 0x00, 0x01))), // flags
-                    box('stbl',
-                        box('stsd',
-                            0x01, // version 1
-                            0x00, 0x00, 0x00, // flags
-                            0x00, 0x00, 0x00, 0x00), // entry_count
-                        box('stts',
-                            0x01, // version 1
-                            0x00, 0x00, 0x00, // flags
-                            0x00, 0x00, 0x00, 0x01, // entry_count
-                            0x00, 0x00, 0x00, 0x01, // sample_count
-                            0x00, 0x00, 0x00, 0x01), // sample_delta
-                        box('stsc',
-                            0x01, // version 1
-                            0x00, 0x00, 0x00, // flags
-                            0x00, 0x00, 0x00, 0x01, // entry_count
-                            0x00, 0x00, 0x00, 0x02, // first_chunk
-                            0x00, 0x00, 0x00, 0x03, // samples_per_chunk
-                            0x00, 0x00, 0x00, 0x01), // sample_description_index
-                        box('stco',
-                            0x01, // version 1
-                            0x00, 0x00, 0x00, // flags
-                            0x00, 0x00, 0x00, 0x01, // entry_count
-                            0x00, 0x00, 0x00, 0x01)))))); // chunk_offset
-
+  var data = mp4Helpers.sampleMoov;
 
   QUnit.deepEqual(mp4.tools.inspect(new Uint8Array(data)), [{
     type: 'moov',
@@ -362,7 +220,7 @@ QUnit.test('can parse a moov', function() {
       flags: new Uint8Array([0, 0, 0]),
       creationTime: new Date(1000 - 2082844800000),
       modificationTime: new Date(2000 - 2082844800000),
-      timescale: 60,
+      timescale: 1000,
       duration: 600,
       rate: 1,
       size: 120,
@@ -396,7 +254,7 @@ QUnit.test('can parse a moov', function() {
           flags: new Uint8Array([0, 0, 0]),
           creationTime: new Date(2000 - 2082844800000),
           modificationTime: new Date(3000 - 2082844800000),
-          timescale: 60,
+          timescale: 90e3,
           duration: 600,
           language: 'eng',
           size: 44
