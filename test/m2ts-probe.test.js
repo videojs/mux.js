4815b636b1c42edc4f39c06e206526e1fa5df00e
Matthew Neil <mneilweb@gmail.com>
2016-09-22 19:14:49 -0400

    Ts inspector (#113)

---
+++ test/m2ts-probe.test.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var
+  QUnit = require('qunit'),
+  probe = require('../lib/m2ts/probe.js'),
+  testSegment = require('./utils/test-segment.js');
+
+/**
+ * All subarray indices verified with the use of thumbcoil.
+ */
+var patPacket = testSegment.subarray(188, 376);
+var pmtPid = 4095;
+var programMapTable = {
+  256: 0x1B,
+  257: 0x0F
+};
+var pmtPacket = testSegment.subarray(376, 564);
+var pesPacket = testSegment.subarray(564, 752);
+var videoPacket = testSegment.subarray(564, 1692);
+var videoNoKeyFramePacket = testSegment.subarray(1880, 2820);
+var audioPacket = testSegment.subarray(6956, 7144);
+var notPusiPacket = testSegment.subarray(1316, 1504);
+
+QUnit.module('M2TS Probe');
+
+QUnit.test('correctly parses packet type', function() {
+  QUnit.equal(probe.parseType(patPacket), 'pat', 'parses pat type');
+  QUnit.equal(probe.parseType(pmtPacket), null,
+    'cannot determine type of pmt packet when pmt pid has not been parsed yet');
+  QUnit.equal(probe.parseType(pmtPacket, pmtPid), 'pmt', 'parses pmt type');
+  QUnit.equal(probe.parseType(pesPacket), null,
+    'cannot determine type of pes packet when pmt pid has not been parsed yet');
+  QUnit.equal(probe.parseType(pesPacket, pmtPid), 'pes', 'parses pes type');
+});
+
+QUnit.test('correctly parses pmt pid from pat packet', function() {
+  QUnit.equal(probe.parsePat(patPacket), pmtPid, 'parses pmt pid from pat');
+});
+
+QUnit.test('correctly parses program map table from pmt packet', function() {
+  QUnit.deepEqual(probe.parsePmt(pmtPacket), programMapTable, 'generates correct pmt');
+});
+
+QUnit.test('correctly parses payload unit start indicator', function() {
+  QUnit.ok(probe.parsePayloadUnitStartIndicator(pesPacket),
+    'detects payload unit start indicator');
+  QUnit.ok(!probe.parsePayloadUnitStartIndicator(notPusiPacket),
+    'detects no payload unit start indicator');
+});
+
+QUnit.test('correctly parses type of pes packet', function() {
+  QUnit.equal(probe.parsePesType(videoPacket, programMapTable), 'video',
+    'parses video pes type');
+  QUnit.equal(probe.parsePesType(audioPacket, programMapTable), 'audio',
+    'parses audio pes type');
+});
+
+QUnit.test('correctly parses dts and pts values of pes packet', function() {
+  var videoPes = probe.parsePesTime(videoPacket);
+  QUnit.equal(videoPes.dts, 126000, 'correct dts value');
+  QUnit.equal(videoPes.pts, 126000, 'correct pts value');
+});
+
+QUnit.test('correctly determines if video pes packet contains a key frame', function() {
+  QUnit.ok(probe.videoPacketContainsKeyFrame(videoPacket), 'detects key frame in packet');
+  QUnit.ok(!probe.videoPacketContainsKeyFrame(videoNoKeyFramePacket),
+    'detects no key frame in packet');
+});
---
+++ test/ts-inspector.test.js
@@ -0,0 +1,138 @@
+'use strict';
+
+var
+  QUnit = require('qunit'),
+  inspect = require('../lib/tools/ts-inspector.js').inspect,
+  tsSegment = require('./utils/test-segment.js'),
+  tsNoAudioSegment = require('./utils/test-no-audio-segment.js'),
+  aacSegment = require('./utils/test-aac-segment.js'),
+  PES_TIMESCALE = 90000;
+
+QUnit.module('TS Inspector');
+
+QUnit.test('returns null for empty segment input', function() {
+  QUnit.equal(inspect(new Uint8Array([])), null, 'returned null');
+});
+
+QUnit.test('can parse a ts segment', function() {
+  var expected = {
+    video: [
+      {
+        type: 'video',
+        pts: 126000 / PES_TIMESCALE,
+        dts: 126000 / PES_TIMESCALE
+      },
+      {
+        type: 'video',
+        pts: 924000 / PES_TIMESCALE,
+        dts: 924000 / PES_TIMESCALE
+      }
+    ],
+    firstKeyFrame: {
+      type: 'video',
+      pts: 126000 / PES_TIMESCALE,
+      dts: 126000 / PES_TIMESCALE
+    },
+    audio: [
+      {
+        type: 'audio',
+        pts: 126000 / PES_TIMESCALE,
+        dts: 126000 / PES_TIMESCALE
+      },
+      {
+        type: 'audio',
+        pts: 859518 / PES_TIMESCALE,
+        dts: 859518 / PES_TIMESCALE
+      }
+    ]
+  };
+
+  QUnit.deepEqual(inspect(tsSegment), expected, 'parses ts segment timing data');
+});
+
+QUnit.test('adjusts timestamp values based on provided reference', function() {
+  var rollover = Math.pow(2, 33);
+
+  var expected = {
+    video: [
+      {
+        type: 'video',
+        pts: (126000 + rollover) / PES_TIMESCALE,
+        dts: (126000 + rollover) / PES_TIMESCALE
+      },
+      {
+        type: 'video',
+        pts: (924000 + rollover) / PES_TIMESCALE,
+        dts: (924000 + rollover) / PES_TIMESCALE
+      }
+    ],
+    firstKeyFrame: {
+      type: 'video',
+      pts: (126000 + rollover) / PES_TIMESCALE,
+      dts: (126000 + rollover) / PES_TIMESCALE
+    },
+    audio: [
+      {
+        type: 'audio',
+        pts: (126000 + rollover) / PES_TIMESCALE,
+        dts: (126000 + rollover) / PES_TIMESCALE
+      },
+      {
+        type: 'audio',
+        pts: (859518 + rollover) / PES_TIMESCALE,
+        dts: (859518 + rollover) / PES_TIMESCALE
+      }
+    ]
+  };
+
+  QUnit.deepEqual(inspect(tsSegment, rollover - 1), expected,
+    'adjusts inspected time data to account for pts rollover');
+});
+
+QUnit.test('can parse an aac segment', function() {
+  var expected = {
+    audio: [
+      {
+        type: 'audio',
+        pts: 895690 / PES_TIMESCALE,
+        dts: 895690 / PES_TIMESCALE
+      },
+      {
+        type: 'audio',
+        pts: (895690 + (430 * 1024 * PES_TIMESCALE / 44100)) / PES_TIMESCALE,
+        dts: (895690 + (430 * 1024 * PES_TIMESCALE / 44100)) / PES_TIMESCALE
+      }
+    ]
+  };
+
+  QUnit.deepEqual(inspect(aacSegment), expected, 'parses aac segment timing data');
+});
+
+QUnit.test('can parse ts segment with no audio muxed in', function() {
+  var expected = {
+    video: [
+      {
+        type: 'video',
+        pts: 126000 / PES_TIMESCALE,
+        dts: 126000 / PES_TIMESCALE
+      },
+      {
+        type: 'video',
+        pts: 924000 / PES_TIMESCALE,
+        dts: 924000 / PES_TIMESCALE
+      }
+    ],
+    firstKeyFrame: {
+      type: 'video',
+      pts: 126000 / PES_TIMESCALE,
+      dts: 126000 / PES_TIMESCALE
+    }
+  };
+
+  var actual = inspect(tsNoAudioSegment);
+
+  QUnit.equal(typeof actual.audio, 'undefined', 'results do not contain audio info');
+  QUnit.deepEqual(actual, expected,
+    'parses ts segment without audio timing data');
+});
+
