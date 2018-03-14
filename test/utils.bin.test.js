9fba7e7befe9d26959c1a9390588ab9f35615954
Matthew Neil <mneil@bcloanerair4.vidmark.local>
2018-03-14 18:53:44 -0400

    fix:mp4 probe bitwise operations

---
+++ test/utils.bin.test.js
@@ -0,0 +1,26 @@
+var
+  QUnit = require('qunit'),
+  toUnsigned = require('../lib/utils/bin').toUnsigned;
+
+QUnit.module('Binary Utils');
+
+QUnit.test('converts values to unsigned integers after bitwise operations', function() {
+var bytes;
+
+bytes = [0, 0, 124, 129];
+
+QUnit.equal(toUnsigned(bytes[0] << 24 |
+                       bytes[1] << 16 |
+                       bytes[2] <<  8 |
+                       bytes[3]),
+            31873, 'positive signed result stays positive');
+
+bytes = [150, 234, 221, 192];
+
+// The following bitwise operation would normally result in -1762992704
+QUnit.equal(toUnsigned(bytes[0] << 24 |
+                       bytes[1] << 16 |
+                       bytes[2] <<  8 |
+                       bytes[3]),
+            2531974592, 'negative signed result becomes unsigned positive');
+});
