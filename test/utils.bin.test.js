b105a4a5c9f68df7a4d049a9e225eee815f88d28
Matthew Neil <mneil@bcloanerair4.vidmark.local>
2018-03-14 19:19:58 -0400

    add assertion

--- test/utils.bin.test.js
+++ test/utils.bin.test.js
@@ -5,22 +5,28 @@ var
 QUnit.module('Binary Utils');
 
 QUnit.test('converts values to unsigned integers after bitwise operations', function() {
-var bytes;
+  var bytes;
 
-bytes = [0, 0, 124, 129];
+  bytes = [0, 0, 124, 129];
 
-QUnit.equal(toUnsigned(bytes[0] << 24 |
-                       bytes[1] << 16 |
-                       bytes[2] <<  8 |
-                       bytes[3]),
-            31873, 'positive signed result stays positive');
+  QUnit.equal(toUnsigned(bytes[0] << 24 |
+                         bytes[1] << 16 |
+                         bytes[2] <<  8 |
+                         bytes[3]),
+              31873, 'positive signed result stays positive');
 
-bytes = [150, 234, 221, 192];
+  bytes = [150, 234, 221, 192];
 
-// The following bitwise operation would normally result in -1762992704
-QUnit.equal(toUnsigned(bytes[0] << 24 |
-                       bytes[1] << 16 |
-                       bytes[2] <<  8 |
-                       bytes[3]),
-            2531974592, 'negative signed result becomes unsigned positive');
+  // sanity check
+  QUnit.equal(bytes[0] << 24 |
+              bytes[1] << 16 |
+              bytes[2] <<  8 |
+              bytes[3],
+              -1762992704, 'bitwise operation produces negative signed result');
+
+  QUnit.equal(toUnsigned(bytes[0] << 24 |
+                         bytes[1] << 16 |
+                         bytes[2] <<  8 |
+                         bytes[3]),
+              2531974592, 'negative signed result becomes unsigned positive');
 });
