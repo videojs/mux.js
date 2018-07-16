7ad13aaa8a9af745c8971b8c5c2ec6bcec69404a
ldayananda <ldayananda@users.noreply.github.com>
2018-07-16 15:44:10 -0400

    feat(captions): parse in-band captions from fmp4 segments (#197)
    * initial parsing out of NAL units
    
    * Fixing syntax error
    
    * Move caption parsing out into another file
    
    * Add parsing/probing of mp4 containers to get caption timing data
    
    * Added parsing out of sample table. Pushed captions onto CaptionStream
    
    * Cleaning up:
    - Move discardEmulationPreventionBytes to captionsParser
    - Cleanup functions in mp4Probe.
      - Now only parseEmbeddedCaptions is exported for captions
      - Now handles multiple traks and ignores non-video traks
      - Renamed functions: captionTracksFromInit -> parseInitForCaptionMetadata, captionTracksFromSegment -> parseSegmentForSamples, captionNals -> parseCaptionNals
    
    * Add troubleshooting guide
    
    * Parse through segment only once:
    - in probe.js, combined functions parseSegmentForSamples and parseCaptionNals.
    - Renamed parseSegmentForSamples -> parseCaptionNals.
    - linting fixes
    
    * Cleaning up in probe.js and created mp4/captions-parser.js:
    - Use existing timescale() method.
    - Only use video track timescales, parse the segment only once rather than for each video track.
    - Moving all caption parsing methods to new file captions-parser.js
    
    * In mp4/captions-parser.js:
    - Using parseTfdt instead of parseDecodeTime
    - Using parseHdlr instead of parseHandlerType
    - Using parseTfhd from mp4Inspector instead
    
    In mp4-inspector.js:
    - Added durationIsEmpty and defaultBaseIsMoof flag handling to parseTfhd
    
    * In mp4/captions-parser.js:
    - Parse samples from all truns
    - Use parseTrun instead of oldParseSamples
    
    * Renamed m2ts/captions-parser.js -> tools/cea708-parser.js
    
    * Sort samples and fail fast if no video traks
    
    * mp4/captions-parser.js: remove unused code and add comments
    tools/mp4-inspector.js: add comments to parse.trun
    
    * mp4-inspector: revert changes to nalParse
    m2ts/caption-stream.js, mp4/captions-parser.js, tools/cea708-parser.js, tools/mp4-inspector.js: clean up imports/exports
    
    * - Added a captions debugging page
    - Added a test content creation doc
    - Updated doc/captions.md with more information
    
    * Update docs
    
    * Adding a few starter tests: one with real content, another with generated content
    
    * Using only needed parts of real test segment. Using correct init test segment.
    
    * Fixing linting errors for test, expanding test a bit
    
    * Use a different segment that can be reduced in size more easily
    
    * Add a bit more to captions doc
    
    * Adding function contracts to: mp4/captions-parser.js
    
    * Use moved test util methods in caption-stream.test.js
    
    * Update test content doc to use a specific format
    
    * update test-content doc
    
    * Update test-content.md
    
    * Fail fast if video track doesn't have captions. Added sensible unit tests
    
    * captions-parser.js: Handle SEIs that match the last sample
    test/captions-parser.test.js: Made a more complex test out of generated test data
    
    * Update to captions-parser:
    - Return both the active streams and the captions from the caption-parser.
    - Update tests
    
    * Use the first video track found only
    
    * captions-parser: fixing bugs - making sure all sample properties have default values
    
    * Update test-content.md
    
    * CR comments:
    - corrections to test-content guide and troubleshooting guide
    - renamed cea708-parser -> caption-packet-parser
    
    * Captions-parser:
    - restructure to persist CaptionStream across segments
    - rework mapping of pts/dts for SEI packets
    - fix tests
    
    * Update test-content.md
    
    * Allow setting an init segment instead of parsing both init and segment each time
    
    * In captions-parser.js: modify parse's signature to take videoTrackIds and timescales
    
    * Use shift instead of splice
    
    * CaptionsParser -> CaptionParser

--- test/caption-stream.test.js
+++ test/caption-stream.test.js
@@ -5,83 +5,13 @@ var
   m2ts = require('../lib/m2ts'),
   mp4 = require('../lib/mp4'),
   QUnit = require('qunit'),
+  seiNalUnitGenerator = require('./utils/sei-nal-unit-generator'),
+  makeSeiFromCaptionPacket = seiNalUnitGenerator.makeSeiFromCaptionPacket,
+  makeSeiFromMultipleCaptionPackets = seiNalUnitGenerator.makeSeiFromMultipleCaptionPackets,
+  characters = seiNalUnitGenerator.characters,
   sintelCaptions = require('./utils/sintel-captions'),
   mixed608708Captions = require('./utils/mixed-608-708-captions'),
   multiChannel608Captions = require('./utils/multi-channel-608-captions');
-
-// Create SEI nal-units from Caption packets
-var makeSeiFromCaptionPacket = function(caption) {
-  return {
-    pts: caption.pts,
-    dts: caption.dts,
-    nalUnitType: 'sei_rbsp',
-    escapedRBSP: new Uint8Array([
-      0x04, // payload_type === user_data_registered_itu_t_t35
-
-      0x0e, // payload_size
-
-      181, // itu_t_t35_country_code
-      0x00, 0x31, // itu_t_t35_provider_code
-      0x47, 0x41, 0x39, 0x34, // user_identifier, "GA94"
-      0x03, // user_data_type_code, 0x03 is cc_data
-
-      // 110 00001
-      0xc1, // process_cc_data, cc_count
-      0xff, // reserved
-      // 1111 1100
-      (0xfc | caption.type), // cc_valid, cc_type (608, field 1)
-      (caption.ccData & 0xff00) >> 8, // cc_data_1
-      caption.ccData & 0xff, // cc_data_2 without parity bit set
-
-      0xff // marker_bits
-    ])
-  };
-};
-
-// Create SEI nal-units from Caption packets
-var makeSeiFromMultipleCaptionPackets = function(captionHash) {
-  var pts = captionHash.pts,
-    dts = captionHash.dts,
-    captions = captionHash.captions;
-
-  var data = [];
-  captions.forEach(function(caption) {
-    data.push(0xfc | caption.type);
-    data.push((caption.ccData & 0xff00) >> 8);
-    data.push(caption.ccData & 0xff);
-  });
-
-  return {
-    pts: pts,
-    dts: dts,
-    nalUnitType: 'sei_rbsp',
-    escapedRBSP: new Uint8Array([
-      0x04, // payload_type === user_data_registered_itu_t_t35
-
-      (0x0b + (captions.length * 3)), // payload_size
-
-      181, // itu_t_t35_country_code
-      0x00, 0x31, // itu_t_t35_provider_code
-      0x47, 0x41, 0x39, 0x34, // user_identifier, "GA94"
-      0x03, // user_data_type_code, 0x03 is cc_data
-
-      // 110 00001
-      (0x6 << 5) | captions.length, // process_cc_data, cc_count
-      0xff // reserved
-    ].concat(data).concat([0xff /* marker bits */])
-    )
-  };
-};
-
-// Returns a ccData byte-pair for a two character string. That is,
-// it converts a string like 'hi' into the two-byte number that
-// would be parsed back as 'hi' when provided as ccData.
-var characters = function(text) {
-  if (text.length !== 2) {
-    throw new Error('ccdata must be specified two characters at a time');
-  }
-  return (text.charCodeAt(0) << 8) | text.charCodeAt(1);
-};
 
 QUnit.module('Caption Stream', {
   beforeEach: function() {
