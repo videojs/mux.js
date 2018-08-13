505d1ad8c183f2d9950c901e3b1907aba06c26ac
Garrett Singer <gesinger@gmail.com>
2018-08-13 10:32:44 -0700

    Reuse more AAC functions (#202)

--- test/aac-stream.test.js
+++ test/aac-stream.test.js
@@ -209,31 +209,6 @@ QUnit.test('handles incomplete adts frame after id3 frame', function() {
   QUnit.equal(id3Count, 1, 'one id3 chunk');
 });
 
-QUnit.test('parses correct ID3 tag size', function() {
-  var
-    packetStream = new Uint8Array(10),
-    parser = aacStream.parseId3TagSize;
-
-  packetStream[9] = 63;
-  QUnit.equal(parser(packetStream, 0),
-              73,
-              'ParseID3 correctly parsed a header without a footer');
-});
-
-QUnit.test('parses correct ADTS Frame size', function() {
-  var
-    packetStream = new Uint8Array(6),
-    parser = aacStream.parseAdtsSize;
-
-  packetStream[3] = 128;
-  packetStream[4] = 29;
-  packetStream[5] = 255;
-
-  QUnit.equal(parser(packetStream, 0),
-              239,
-              'ParseADTS correctly parsed framesize');
-});
-
 QUnit.test('emits data after receiving push', function() {
   var
     array = new Uint8Array(109),
