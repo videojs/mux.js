b07374dcfc452a4286af60aec2b5552239aa5dca
brandonocasey <brandonocasey@gmail.com>
2020-02-07 12:10:51 -0500

    better tests, bring #285 to the partial transmuxer

--- test/partial.test.js
+++ test/partial.test.js
@@ -7,365 +7,130 @@ var packetize = utils.packetize;
 var PAT = utils.PAT;
 
 QUnit.module('Partial Transmuxer - Options');
-QUnit.test('Audio frames trimmed before video, keepOriginalTimestamps = false', function() {
-  var
-    segments = [],
-    earliestDts = 15000,
-    transmuxer = new Transmuxer({
-      keepOriginalTimestamps: false
-    });
+[
+  {options: {keepOriginalTimestamps: false}},
+  {options: {keepOriginalTimestamps: true}},
+  {options: {keepOriginalTimestamps: false, baseMediaDecodeTime: 15000}},
+  {options: {keepOriginalTimestamps: true, baseMediaDecodeTime: 15000}},
+  {options: {keepOriginalTimestamps: false}, baseMediaSetter: 15000},
+  {options: {keepOriginalTimestamps: true}, baseMediaSetter: 15000}
+].forEach(function(test) {
+  var createTransmuxer = function() {
+    var transmuxer = new Transmuxer(test.options);
 
-  transmuxer.on('data', function(segment) {
-    segments.push(segment);
+    if (test.baseMediaSetter) {
+      transmuxer.setBaseMediaDecodeTime(test.baseMediaSetter);
+    }
+
+    return transmuxer;
+  };
+
+  var name = '';
+
+  Object.keys(test.options).forEach(function(optionName) {
+    name += '' + optionName + ' ' + test.options[optionName] + ' ';
   });
 
-  transmuxer.push(packetize(PAT));
-  transmuxer.push(packetize(generatePMT({
-    hasVideo: true,
-    hasAudio: true
-  })));
+  if (test.baseMediaSetter) {
+    name += 'baseMediaDecodeTime setter ' + test.baseMediaSetter;
+  }
 
-  transmuxer.push(packetize(audioPes([
-    0x19, 0x47
-  ], true, earliestDts - 1)));
-
-  transmuxer.push(packetize(videoPes([
-      0x09, 0x01 // access_unit_delimiter_rbsp
-  ], true, earliestDts)));
-  transmuxer.push(packetize(videoPes([
-      0x08, 0x01 // pic_parameter_set_rbsp
-  ], true, earliestDts)));
-  transmuxer.push(packetize(videoPes([
-    0x07, // seq_parameter_set_rbsp
-    0x27, 0x42, 0xe0, 0x0b,
-    0xa9, 0x18, 0x60, 0x9d,
-    0x80, 0x53, 0x06, 0x01,
-    0x06, 0xb6, 0xc2, 0xb5,
-    0xef, 0x7c, 0x04
-  ], false, earliestDts)));
-  transmuxer.push(packetize(videoPes([
-      0x05, 0x01 // slice_layer_without_partitioning_rbsp_idr
-  ], true, earliestDts)));
-
-  transmuxer.flush();
-
-  QUnit.equal(segments.length, 1, 'generated only a video segment');
-});
-
-QUnit.test('Audio frames trimmed before video, keepOriginalTimestamps = true', function() {
-  var
+  QUnit.test('Audio frames after video not trimmed, ' + name, function() {
+    var
     segments = [],
-    earliestDts = 15000,
-    transmuxer = new Transmuxer({
-      keepOriginalTimestamps: true
+      earliestDts = 15000,
+      transmuxer = createTransmuxer();
+
+    transmuxer.on('data', function(segment) {
+      segments.push(segment);
     });
 
-  transmuxer.on('data', function(segment) {
-    segments.push(segment);
+    // the following transmuxer pushes add tiny video and
+    // audio data to the transmuxer. When we add the data
+    // we also set the pts/dts time so that audio should
+    // not be trimmed.
+    transmuxer.push(packetize(PAT));
+    transmuxer.push(packetize(generatePMT({
+      hasVideo: true,
+      hasAudio: true
+    })));
+
+    transmuxer.push(packetize(audioPes([
+      0x19, 0x47
+    ], true, earliestDts + 1)));
+    transmuxer.push(packetize(videoPes([
+      0x09, 0x01 // access_unit_delimiter_rbsp
+    ], true, earliestDts)));
+    transmuxer.push(packetize(videoPes([
+      0x08, 0x01 // pic_parameter_set_rbsp
+    ], true, earliestDts)));
+    transmuxer.push(packetize(videoPes([
+      0x07, // seq_parameter_set_rbsp
+      0x27, 0x42, 0xe0, 0x0b,
+      0xa9, 0x18, 0x60, 0x9d,
+      0x80, 0x53, 0x06, 0x01,
+      0x06, 0xb6, 0xc2, 0xb5,
+      0xef, 0x7c, 0x04
+    ], false, earliestDts)));
+    transmuxer.push(packetize(videoPes([
+      0x05, 0x01 // slice_layer_without_partitioning_rbsp_idr
+    ], true, earliestDts)));
+    transmuxer.flush();
+
+    // the partial transmuxer only generates a video segment
+    // when all audio frames are trimmed. So we should have an audio and video
+    // segment
+    QUnit.equal(segments.length, 2, 'generated a video and an audio segment');
+    QUnit.equal(segments[0].type, 'video', 'video segment exists');
+    QUnit.equal(segments[1].type, 'audio', 'audio segment exists');
   });
 
-  transmuxer.push(packetize(PAT));
-  transmuxer.push(packetize(generatePMT({
-    hasVideo: true,
-    hasAudio: true
-  })));
-
-  transmuxer.push(packetize(audioPes([
-    0x19, 0x47
-  ], true, earliestDts - 1)));
-  transmuxer.push(packetize(videoPes([
-      0x09, 0x01 // access_unit_delimiter_rbsp
-  ], true, earliestDts)));
-  transmuxer.push(packetize(videoPes([
-      0x08, 0x01 // pic_parameter_set_rbsp
-  ], true, earliestDts)));
-  transmuxer.push(packetize(videoPes([
-    0x07, // seq_parameter_set_rbsp
-    0x27, 0x42, 0xe0, 0x0b,
-    0xa9, 0x18, 0x60, 0x9d,
-    0x80, 0x53, 0x06, 0x01,
-    0x06, 0xb6, 0xc2, 0xb5,
-    0xef, 0x7c, 0x04
-  ], false, earliestDts)));
-  transmuxer.push(packetize(videoPes([
-      0x05, 0x01 // slice_layer_without_partitioning_rbsp_idr
-  ], true, earliestDts)));
-  transmuxer.flush();
-
-  QUnit.equal(segments.length, 1, 'generated only a video segment');
-});
-
-QUnit.test('Audio frames after video not trimmed, keepOriginalTimestamps = false', function() {
-  var
+  QUnit.test('Audio frames trimmed before video, ' + name, function() {
+    var
     segments = [],
-    earliestDts = 15000,
-    transmuxer = new Transmuxer({
-      keepOriginalTimestamps: false
+      earliestDts = 15000,
+      transmuxer = createTransmuxer();
+
+    transmuxer.on('data', function(segment) {
+      segments.push(segment);
     });
 
-  transmuxer.on('data', function(segment) {
-    segments.push(segment);
-  });
+    // the following transmuxer pushes add tiny video and
+    // audio data to the transmuxer. When we add the data
+    // we also set the pts/dts time so that audio should
+    // be trimmed.
+    transmuxer.push(packetize(PAT));
+    transmuxer.push(packetize(generatePMT({
+      hasVideo: true,
+      hasAudio: true
+    })));
 
-  transmuxer.push(packetize(PAT));
-  transmuxer.push(packetize(generatePMT({
-    hasVideo: true,
-    hasAudio: true
-  })));
-
-  transmuxer.push(packetize(audioPes([
-    0x19, 0x47
-  ], true, earliestDts + 1)));
-  transmuxer.push(packetize(videoPes([
+    transmuxer.push(packetize(audioPes([
+      0x19, 0x47
+    ], true, earliestDts - (test.options.baseMediaDecodeTime || test.baseMediaSetter || 0) - 1)));
+    transmuxer.push(packetize(videoPes([
       0x09, 0x01 // access_unit_delimiter_rbsp
-  ], true, earliestDts)));
-  transmuxer.push(packetize(videoPes([
+    ], true, earliestDts)));
+    transmuxer.push(packetize(videoPes([
       0x08, 0x01 // pic_parameter_set_rbsp
-  ], true, earliestDts)));
-  transmuxer.push(packetize(videoPes([
-    0x07, // seq_parameter_set_rbsp
-    0x27, 0x42, 0xe0, 0x0b,
-    0xa9, 0x18, 0x60, 0x9d,
-    0x80, 0x53, 0x06, 0x01,
-    0x06, 0xb6, 0xc2, 0xb5,
-    0xef, 0x7c, 0x04
-  ], false, earliestDts)));
-  transmuxer.push(packetize(videoPes([
+    ], true, earliestDts)));
+    transmuxer.push(packetize(videoPes([
+      0x07, // seq_parameter_set_rbsp
+      0x27, 0x42, 0xe0, 0x0b,
+      0xa9, 0x18, 0x60, 0x9d,
+      0x80, 0x53, 0x06, 0x01,
+      0x06, 0xb6, 0xc2, 0xb5,
+      0xef, 0x7c, 0x04
+    ], false, earliestDts)));
+    transmuxer.push(packetize(videoPes([
       0x05, 0x01 // slice_layer_without_partitioning_rbsp_idr
-  ], true, earliestDts)));
-  transmuxer.flush();
+    ], true, earliestDts)));
+    transmuxer.flush();
 
-  QUnit.equal(segments.length, 2, 'generated a video and an audio segment');
-  QUnit.equal(segments[1].data.boxes.length, 122, 'trimmed audio frame');
-});
-
-QUnit.test('Audio frames after video not trimmed, keepOriginalTimestamps = true', function() {
-  var
-    segments = [],
-    earliestDts = 15000,
-    transmuxer = new Transmuxer({
-      keepOriginalTimestamps: true
-    });
-
-  transmuxer.on('data', function(segment) {
-    segments.push(segment);
+    // the partial transmuxer only generates a video segment
+    // when all audio frames are trimmed. So we should only have
+    // a video segment
+    QUnit.equal(segments.length, 1, 'generated only a video segment');
+    QUnit.equal(segments[0].type, 'video', 'segment is video');
   });
-
-  transmuxer.push(packetize(PAT));
-  transmuxer.push(packetize(generatePMT({
-    hasVideo: true,
-    hasAudio: true
-  })));
-
-  transmuxer.push(packetize(audioPes([
-    0x19, 0x47
-  ], true, earliestDts + 1)));
-  transmuxer.push(packetize(videoPes([
-      0x09, 0x01 // access_unit_delimiter_rbsp
-  ], true, earliestDts)));
-  transmuxer.push(packetize(videoPes([
-      0x08, 0x01 // pic_parameter_set_rbsp
-  ], true, earliestDts)));
-  transmuxer.push(packetize(videoPes([
-    0x07, // seq_parameter_set_rbsp
-    0x27, 0x42, 0xe0, 0x0b,
-    0xa9, 0x18, 0x60, 0x9d,
-    0x80, 0x53, 0x06, 0x01,
-    0x06, 0xb6, 0xc2, 0xb5,
-    0xef, 0x7c, 0x04
-  ], false, earliestDts)));
-  transmuxer.push(packetize(videoPes([
-      0x05, 0x01 // slice_layer_without_partitioning_rbsp_idr
-  ], true, earliestDts)));
-  transmuxer.flush();
-
-  QUnit.equal(segments.length, 2, 'generated a video and an audio segment');
-  QUnit.equal(segments[1].data.boxes.length, 122, 'trimmed audio frame');
 });
-
-QUnit.test('Audio frames after video not trimmed, keepOriginalTimestamps = false, baseMediaDecodeTime option', function() {
-  var
-    segments = [],
-    earliestDts = 15000,
-    baseMediaDecodeTime = 15000,
-    transmuxer = new Transmuxer({
-      keepOriginalTimestamps: false,
-      baseMediaDecodeTime: baseMediaDecodeTime
-    });
-
-  transmuxer.on('data', function(segment) {
-    segments.push(segment);
-  });
-
-  transmuxer.push(packetize(PAT));
-  transmuxer.push(packetize(generatePMT({
-    hasVideo: true,
-    hasAudio: true
-  })));
-
-  transmuxer.push(packetize(audioPes([
-    0x19, 0x47
-  ], true, earliestDts + 1)));
-  transmuxer.push(packetize(videoPes([
-      0x09, 0x01 // access_unit_delimiter_rbsp
-  ], true, earliestDts + baseMediaDecodeTime)));
-  transmuxer.push(packetize(videoPes([
-      0x08, 0x01 // pic_parameter_set_rbsp
-  ], true, earliestDts + baseMediaDecodeTime)));
-  transmuxer.push(packetize(videoPes([
-    0x07, // seq_parameter_set_rbsp
-    0x27, 0x42, 0xe0, 0x0b,
-    0xa9, 0x18, 0x60, 0x9d,
-    0x80, 0x53, 0x06, 0x01,
-    0x06, 0xb6, 0xc2, 0xb5,
-    0xef, 0x7c, 0x04
-  ], false, earliestDts + baseMediaDecodeTime)));
-  transmuxer.push(packetize(videoPes([
-      0x05, 0x01 // slice_layer_without_partitioning_rbsp_idr
-  ], true, earliestDts + baseMediaDecodeTime)));
-  transmuxer.flush();
-
-  QUnit.equal(segments.length, 2, 'generated a video and an audio segment');
-  QUnit.equal(segments[1].data.boxes.length, 122, 'trimmed audio frame');
-});
-
-QUnit.test('Audio frames after video not trimmed, keepOriginalTimestamps = true, baseMediaDecodeTime option', function() {
-  var
-    segments = [],
-    earliestDts = 15000,
-    baseMediaDecodeTime = 15000,
-    transmuxer = new Transmuxer({
-      keepOriginalTimestamps: true,
-      baseMediaDecodeTime: baseMediaDecodeTime
-    });
-
-  transmuxer.on('data', function(segment) {
-    segments.push(segment);
-  });
-
-  transmuxer.push(packetize(PAT));
-  transmuxer.push(packetize(generatePMT({
-    hasVideo: true,
-    hasAudio: true
-  })));
-
-  transmuxer.push(packetize(audioPes([
-    0x19, 0x47
-  ], true, earliestDts + 1)));
-  transmuxer.push(packetize(videoPes([
-      0x09, 0x01 // access_unit_delimiter_rbsp
-  ], true, earliestDts + baseMediaDecodeTime)));
-  transmuxer.push(packetize(videoPes([
-      0x08, 0x01 // pic_parameter_set_rbsp
-  ], true, earliestDts + baseMediaDecodeTime)));
-  transmuxer.push(packetize(videoPes([
-    0x07, // seq_parameter_set_rbsp
-    0x27, 0x42, 0xe0, 0x0b,
-    0xa9, 0x18, 0x60, 0x9d,
-    0x80, 0x53, 0x06, 0x01,
-    0x06, 0xb6, 0xc2, 0xb5,
-    0xef, 0x7c, 0x04
-  ], false, earliestDts + baseMediaDecodeTime)));
-  transmuxer.push(packetize(videoPes([
-      0x05, 0x01 // slice_layer_without_partitioning_rbsp_idr
-  ], true, earliestDts + baseMediaDecodeTime)));
-  transmuxer.flush();
-
-  QUnit.equal(segments.length, 2, 'generated a video and an audio segment');
-  QUnit.equal(segments[1].data.boxes.length, 122, 'trimmed audio frame');
-});
-
-QUnit.test('Audio frames after video not trimmed, keepOriginalTimestamps = false, baseMediaDecodeTime setter', function() {
-  var
-    segments = [],
-    earliestDts = 15000,
-    baseMediaDecodeTime = 15000,
-    transmuxer = new Transmuxer({
-      keepOriginalTimestamps: false
-    });
-
-  transmuxer.setBaseMediaDecodeTime(baseMediaDecodeTime);
-
-  transmuxer.on('data', function(segment) {
-    segments.push(segment);
-  });
-
-  transmuxer.push(packetize(PAT));
-  transmuxer.push(packetize(generatePMT({
-    hasVideo: true,
-    hasAudio: true
-  })));
-
-  transmuxer.push(packetize(audioPes([
-    0x19, 0x47
-  ], true, earliestDts + 1)));
-  transmuxer.push(packetize(videoPes([
-      0x09, 0x01 // access_unit_delimiter_rbsp
-  ], true, earliestDts + baseMediaDecodeTime)));
-  transmuxer.push(packetize(videoPes([
-      0x08, 0x01 // pic_parameter_set_rbsp
-  ], true, earliestDts + baseMediaDecodeTime)));
-  transmuxer.push(packetize(videoPes([
-    0x07, // seq_parameter_set_rbsp
-    0x27, 0x42, 0xe0, 0x0b,
-    0xa9, 0x18, 0x60, 0x9d,
-    0x80, 0x53, 0x06, 0x01,
-    0x06, 0xb6, 0xc2, 0xb5,
-    0xef, 0x7c, 0x04
-  ], false, earliestDts + baseMediaDecodeTime)));
-  transmuxer.push(packetize(videoPes([
-      0x05, 0x01 // slice_layer_without_partitioning_rbsp_idr
-  ], true, earliestDts + baseMediaDecodeTime)));
-  transmuxer.flush();
-
-  QUnit.equal(segments.length, 2, 'generated a video and an audio segment');
-  QUnit.equal(segments[1].data.boxes.length, 122, 'trimmed audio frame');
-});
-
-QUnit.test('Audio frames after video not trimmed, keepOriginalTimestamps = true, baseMediaDecodeTime setter', function() {
-  var
-    segments = [],
-    earliestDts = 15000,
-    baseMediaDecodeTime = 15000,
-    transmuxer = new Transmuxer({
-      keepOriginalTimestamps: true
-    });
-
-  transmuxer.setBaseMediaDecodeTime(baseMediaDecodeTime);
-
-  transmuxer.on('data', function(segment) {
-    segments.push(segment);
-  });
-
-  transmuxer.push(packetize(PAT));
-  transmuxer.push(packetize(generatePMT({
-    hasVideo: true,
-    hasAudio: true
-  })));
-
-  transmuxer.push(packetize(audioPes([
-    0x19, 0x47
-  ], true, earliestDts + 1)));
-  transmuxer.push(packetize(videoPes([
-      0x09, 0x01 // access_unit_delimiter_rbsp
-  ], true, earliestDts + baseMediaDecodeTime)));
-  transmuxer.push(packetize(videoPes([
-      0x08, 0x01 // pic_parameter_set_rbsp
-  ], true, earliestDts + baseMediaDecodeTime)));
-  transmuxer.push(packetize(videoPes([
-    0x07, // seq_parameter_set_rbsp
-    0x27, 0x42, 0xe0, 0x0b,
-    0xa9, 0x18, 0x60, 0x9d,
-    0x80, 0x53, 0x06, 0x01,
-    0x06, 0xb6, 0xc2, 0xb5,
-    0xef, 0x7c, 0x04
-  ], false, earliestDts + baseMediaDecodeTime)));
-  transmuxer.push(packetize(videoPes([
-      0x05, 0x01 // slice_layer_without_partitioning_rbsp_idr
-  ], true, earliestDts + baseMediaDecodeTime)));
-  transmuxer.flush();
-
-  QUnit.equal(segments.length, 2, 'generated a video and an audio segment');
-  QUnit.equal(segments[1].data.boxes.length, 122, 'trimmed audio frame');
-});
-
