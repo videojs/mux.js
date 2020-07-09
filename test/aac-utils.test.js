11ef93625625986e3cfcc4c0c8939541be4ba8fc
brandonocasey <brandonocasey@gmail.com>
2020-07-09 13:31:01 -0400

    failing test

--- test/aac-utils.test.js
+++ test/aac-utils.test.js
@@ -11,6 +11,15 @@ var audioFrameOffset = 73;
 
 QUnit.module('AAC Utils');
 
+QUnit.test('correctly determines aac data', function(assert) {
+  assert.ok(utils.isLikelyAacData(testSegment), 'test segment is aac');
+
+  var id3Offset = utils.parseId3TagSize(testSegment, 0);
+
+  assert.ok(utils.isLikelyAacData(testSegment.subarray(id3Offset)), 'test segment is aac without id3');
+});
+
+
 QUnit.test('correctly parses aac packet type', function() {
   QUnit.equal(utils.parseType(testSegment, id3TagOffset), 'timed-metadata',
     'parsed timed-metadata type');
