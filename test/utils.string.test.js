8bfc3ce015e75306975d489f41532a3ae8b6fc28
Adam Waldron <awaldron@brightcove.com>
2023-01-24 10:37:46 -0800

    fix tab spaces and alignment

--- test/utils.string.test.js
+++ test/utils.string.test.js
@@ -7,12 +7,12 @@ var
 QUnit.module('String Utils');
 
 QUnit.test('Converts a uint8 array into a C string from start of array until first null char', function(assert) {
-    var uint8String = new Uint8Array([0x66, 0x6F, 0x6F, 0x2E, 0x62, 0x61, 0x72, 0x2E, 0x76, 0x61, 0x6C, 0x75, 0x65, 0x00,
-        0x76, 0x61, 0x6C, 0x75, 0x65, 0x2E, 0x62, 0x61, 0x72, 0x00]); // foo.bar.value\0value.bar\0
-    var firstString = string.uint8ToCString(uint8String);
-    assert.equal(firstString, 'foo.bar.value\0', 'converts uint8 data to a c string');
-    assert.equal(firstString.length, 14, 'string has the correct length');
-    var secondString = string.uint8ToCString(uint8String.subarray(14));
-    assert.equal(secondString, 'value.bar\0', 'converts uint8 data to a c string');
-    assert.equal(secondString.length, 10, 'string has the correct length');
+  var uint8String = new Uint8Array([0x66, 0x6F, 0x6F, 0x2E, 0x62, 0x61, 0x72, 0x2E, 0x76, 0x61, 0x6C, 0x75, 0x65, 0x00,
+    0x76, 0x61, 0x6C, 0x75, 0x65, 0x2E, 0x62, 0x61, 0x72, 0x00]); // foo.bar.value\0value.bar\0
+  var firstString = string.uint8ToCString(uint8String);
+  assert.equal(firstString, 'foo.bar.value\0', 'converts uint8 data to a c string');
+  assert.equal(firstString.length, 14, 'string has the correct length');
+  var secondString = string.uint8ToCString(uint8String.subarray(14));
+  assert.equal(secondString, 'value.bar\0', 'converts uint8 data to a c string');
+  assert.equal(secondString.length, 10, 'string has the correct length');
 });
